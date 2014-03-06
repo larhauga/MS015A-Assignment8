640edb3f245b279ef5d7b0e666b18ecc3492b511
Lars Haugan <lars@larshaugan.net>
2014-03-06 10:05:03 +0100

    Changed to the simple python implementation

--- simple_smtp.py
+++ simple_smtp.py
@@ -5,27 +5,17 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 def snmp_get(host):
     community = 'public'
 
-    cmdGen = cmdgen.CommandGenerator()
+    cmdg = cmdgen.CommandGenerator()
+    commdata = cmdgen.CommunityData('username','public')
+    transport = cmdgen.UdpTransportTarget((host, 161))
+    variables = (1,3,6,1,2,1,1,1,0)
+    err_ind, err_status, err_index, result = cmdg.getCmd(commdata, transport,
+            variables)
+    print err_ind
+    print err_index
+    print err_status
+    print result
 
-    errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-        cmdgen.CommunityData(community),
-        cmdgen.UdpTransportTarget((host, 161)),
-        cmdgen.MibVariable('SNMPv2-MIB', 'sysName', 0)
-    )
-
-    # Check for errors and print out results
-    if errorIndication:
-        print(errorIndication)
-    else:
-        if errorStatus:
-            print('%s at %s' % (
-                errorStatus.prettyPrint(),
-                errorIndex and varBinds[int(errorIndex)-1] or '?'
-                )
-            )
-        else:
-            for name, val in varBinds:
-                print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
 
 if __name__ == '__main__':
     print snmp_get("localhost")
