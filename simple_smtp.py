523183e91243fff3c69282837632cb761ac15f07
Lars Haugan <lars@larshaugan.net>
2014-03-06 10:14:08 +0100

    Added return variables and added parameter for the variable in get functon

--- simple_smtp.py
+++ simple_smtp.py
@@ -2,25 +2,34 @@
 
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
-def snmp_get(host):
+def snmp_get(host, variables):
     community = 'public'
 
     cmdg = cmdgen.CommandGenerator()
     commdata = cmdgen.CommunityData('username','public')
     transport = cmdgen.UdpTransportTarget((host, 161))
-    variables = (1,3,6,1,2,1,1,1,0)
     err_ind, err_status, err_index, result = cmdg.getCmd(commdata, transport,
             variables)
 
     if err_ind:
         print(err_ind)
+        return None
     else:
         if err_status:
             print('%s at %s' % (err_status.prettyPrint(),
                             error_ind and result[int(errorIndex)-1]) or '?')
+            return None
         else:
             for name, val in result:
                 print("%s: %s" % (name.prettyPrint(), val.prettyPrint()))
+            return result
 
 if __name__ == '__main__':
-    print snmp_get("localhost")
+    host    = "localhost"
+    cpu1min = (1,3,6,1,4,1,2021,10,1,3,1)
+    cpu5min = (1,3,6,1,4,1,2021,10,1,3,2)
+    cpu15min= (1,3,6,1,4,1,2021,10,1,3,3)
+    hostname= (1,3,6,1,2,1,1,1,0)
+
+    print snmp_get(host, hostname)
+    print snmp_get(host)
